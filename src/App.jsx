8115a4066c9f30045e56c825ee4463d69a3e29bc
mohima-mimino11 <mohimahoque22@gmail.com>
2025-02-04 23:02:51 +0600

    layout design of project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
   
 
   return (
-    <>
+    <div>
      <Header></Header>
-     <Blogs></Blogs>
-    </>
+     <div className='md:flex px-[320px] mt-8'>
+       <Blogs></Blogs>
+       <Bookmarks></Bookmarks>
+     </div>
+    </div>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import './Header.css';
+import profile from '../../assets/images/profile.png'
 const Header = () => {
   return (
-    <div className='px-[320px] mt-12'>
-       <h1 className='text-4xl exo-2 font-bold'>Knowledg Cafe</h1>
+    <div className='px-[320px] mt-12 flex justify-between items-center border-b-2 border-gray-300'>
+       <h1 className='text-4xl exo-2 font-bold mb-8'>Knowledge Cafe</h1>
+       <img src={profile} className='mb-8' alt="" />
       
     </div>
   );
