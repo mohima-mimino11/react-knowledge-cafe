bc53a7852f678c53a23ab8add54d305e5543b3a8
mohima-mimino11 <mohimahoque22@gmail.com>
2025-02-04 22:29:45 +0600

    project setup and json data fetch done

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect  } from "react";
+
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(()=>{
+    fetch('blogs.json')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  },[])
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+const Header = () => {
+  return (
+    <div className='px-[320px] mt-12'>
+       <h1 className='text-4xl exo-2 font-bold'>Knowledg Cafe</h1>
+      
+    </div>
+  );
+};
+
+export default Header;
